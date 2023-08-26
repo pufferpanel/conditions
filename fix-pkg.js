559ccbf99012c51283cc93c864b0cb8b9e38e6b0
nepcore <nepcore@griem.xyz>
2023-08-26 18:35:52 +0200

    yet another attempt at fixing tests and build output

---
+++ fix-pkg.js
@@ -0,0 +1,16 @@
+// take package.json from repo root and add wasm-packs output files
+// write that back to the wasm-pack output dir to make nodejs based test work
+ 
+const fs = require('fs')
+
+const rootPkgFile = fs.readFileSync('package.json', 'utf-8')
+const rootPkg = JSON.parse(rootPkgFile)
+
+const wasmPkgFile = fs.readFileSync('./pkg/package.json', 'utf-8')
+const wasmPkg = JSON.parse(wasmPkgFile)
+
+rootPkg.files = wasmPkg.files
+rootPkg.main = wasmPkg.main
+rootPkg.types = wasmPkg.types
+
+fs.writeFileSync('./pkg/package.json', JSON.stringify(rootPkg, null, 2), 'utf-8')
